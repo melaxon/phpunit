3e274317e74608530cfc29483c90b5ad0fdd87ca
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-02-07 15:44:47 +0100

    Closes #5311

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -1359,7 +1359,6 @@ abstract class TestCase extends Assert implements Reorderable, SelfDescribing, T
             ->disableOriginalConstructor()
             ->disableOriginalClone()
             ->disableArgumentCloning()
-            ->disallowMockingUnknownTypes()
             ->onlyMethods($methods)
             ->getMock();
 
