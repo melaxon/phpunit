8b77dfeaa42007879bded1219f7484cbd7b00031
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-02-08 08:21:01 +0100

    Add missing annotations for exceptions

--- src/Framework/MockObject/Generator/Generator.php
+++ src/Framework/MockObject/Generator/Generator.php
@@ -84,6 +84,7 @@ final class Generator
      * @throws ClassIsReadonlyException
      * @throws DuplicateMethodException
      * @throws InvalidMethodNameException
+     * @throws MethodNamedMethodException
      * @throws ReflectionException
      * @throws RuntimeException
      * @throws UnknownTypeException
@@ -189,6 +190,7 @@ final class Generator
      * @throws ClassIsEnumerationException
      * @throws ClassIsFinalException
      * @throws ClassIsReadonlyException
+     * @throws MethodNamedMethodException
      * @throws ReflectionException
      * @throws RuntimeException
      *
